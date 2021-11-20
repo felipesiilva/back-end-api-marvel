6d573de70dc14fc3565bc3d09b4b80e4b0412937
Felipe Silva <73503676+felipesiilva@users.noreply.github.com>
2021-11-19 22:10:04 -0300

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,102 @@
+import express, { Request, Response, NextFunction } from "express";
+import axios from "axios";
+import cors from "cors";
+import md5 from "md5";
+import "dotenv/config";
+
+const privateKey = process.env.PRIVATE_KEY;
+const publicKey = process.env.PUBLIC_KEY;
+const apiUrl = "http://gateway.marvel.com/v1/public";
+
+const app = express();
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`started server on PORT:${port} `);
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/personagem", (req: Request, res: Response, next: NextFunction) => {
+  const page: number = Number(req.query.page) - 1; //validar
+  const limit: number = Number(req.query.limit); //validar
+  const ts = new Date().getTime().toString();
+  const hash = md5(ts + privateKey + publicKey);
+
+  axios
+    .get(`${apiUrl}/characters`, {
+      params: {
+        ts: ts,
+        apikey: publicKey,
+        hash: hash,
+        orderBy: "name",
+        limit: limit,
+        offset: page * limit,
+      },
+    })
+    .then((response) => {
+      const personagens: Array<any> = response.data.data.results;
+      const nomes: Array<any> = personagens.map((personagem) => {
+        return {
+          nome: personagem.name,
+          id: personagem.id,
+        };
+      });
+      const objRetorno = {
+        page: page + 1,
+        count: nomes.length,
+        totalPages: Math.floor(response.data.data.total / limit + 1),
+        personagens: [...nomes],
+      };
+      res.json(objRetorno);
+    })
+    .catch((err) => {
+      console.log(err.message);
+      res.status(500).send("Internal error");
+    });
+});
+
+app.get(
+  "/personagem/:id",
+  (req: Request, res: Response, next: NextFunction) => {
+    const ts = new Date().getTime().toString();
+    const hash = md5(ts + privateKey + publicKey);
+
+    const idPersonagem: number = Number(req.params.id);
+
+    axios
+      .get(`${apiUrl}/characters/${idPersonagem}`, {
+        params: {
+          ts: ts,
+          apikey: publicKey,
+          hash: hash,
+        },
+      })
+      .then((response) => {
+        const imagem: string = response.data.data.results[0].thumbnail;
+        const nome: string = response.data.data.results[0].name;
+        let descricao: string = response.data.data.results[0].description;
+        const hqs: any = response.data.data.results[0].comics.items.map(
+          (item: { name: any; resourceURI: any }) => {
+            return { name: item.name, uri: item.resourceURI };
+          }
+        );
+
+        if (!descricao) {
+          descricao = "Não informado";
+        }
+        //else if ()
+
+        console.log(hqs);
+
+        return res.json({
+          img: imagem,
+          nome: nome,
+          descricao: descricao,
+          hqs: hqs,
+        });
+      });
+  }
+);
